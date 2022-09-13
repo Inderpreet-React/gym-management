845a28c7fcc5a2966a67995de15e72d022276d4e
Inderpreet <inderpreet.react.project@gmail.com>
2022-09-13 23:52:14 +0530

    updated  routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashBoardPage from "./pages/DashBoardPage";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LoginPage />} />
-				<Route path="/dashboard" element={<DashBoardPage />}></Route>
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			<Route path="/" element={<LoginPage />} />
+			<Route path="/dashboard" element={<DashBoardPage />} />
+			<Route path="*" element={<PageNotFound />} />
+		</Routes>
 	);
 }
 
