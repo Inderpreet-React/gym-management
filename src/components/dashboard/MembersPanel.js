3ae24de6755b6a4f89028ff44e8806ebea327eb6
Inderpreet <inderpreet.react.project@gmail.com>
2022-09-24 22:00:15 +0530

    Members panel tabled design added

--- src/components/dashboard/MembersPanel.js
+++ src/components/dashboard/MembersPanel.js
@@ -8,26 +8,20 @@ export default function MembersPanel() {
 	return (
 		<div className="h-full w-full bg-white p-4 shadow-md">
 			<div className="h-full w-full overflow-hidden rounded border-2 border-indigo-400 p-2">
-				<table className="w-full  text-center text-lg">
-					<thead className="h-4 w-full rounded-t bg-gray-400  py-4 text-white">
-						<tr className="">
-							<th>Name</th>
-							<th>Current Subscription Starting</th>
-							<th>Current Subscription Ending</th>
-							<th>Update Subscription</th>
+				<table className="w-full table-auto overflow-hidden rounded text-center text-lg">
+					<thead className="h-4 w-full  bg-indigo-500  py-4 uppercase text-white">
+						<tr>
+							<th className="py-2">Name</th>
+							<th>Current Plan</th>
+							<th>Joined On</th>
+							<th>Plan Starting Date</th>
+							<th>Plan Ending Date</th>
 						</tr>
 					</thead>
-					<tbody>
-						<TdMember
-							name={"Simarjeet Singh"}
-							startingDate={"12 Dec 2021"}
-							endingDate={"12 Jan 2022"}
-						/>
-						<TdMember
-							name={"Ayush"}
-							startingDate={"22 Dec 2021"}
-							endingDate={"22 March 2022"}
-						/>
+					<tbody className="text-gray-700">
+						{allMembers.map((member, key) => {
+							return <TdMember key={key} member={member} />;
+						})}
 					</tbody>
 				</table>
 			</div>
--- src/components/dashboard/TdMember.js
+++ src/components/dashboard/TdMember.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 export default function TdMember(props) {
+	const name = props.member.name;
+	const joinedOn = props.member.joiningDate.toDate();
+	const currentPlanStartingDate = props.member.currentPlanStartingDate.toDate();
+	const currentPlanEndingDate = props.member.currentPlanEndingDate.toDate();
+	const currentSubscriptionPlan = props.member.currentSubscriptionPlan;
+
+	const tdClassName = "py-2";
+
 	return (
-		<tr>
-			<td>{props.name}</td>
-			<td>{props.startingDate}</td>
-			<td>{props.endingDate}</td>
-			<td className="text-indigo-500 underline">Update</td>
+		<tr className="odd:bg-gray-200 even:bg-gray-100">
+			<td className={tdClassName}>{name}</td>
+			<td className={tdClassName}>{`${currentSubscriptionPlan} Months`}</td>
+			<td
+				className={tdClassName}
+			>{`${joinedOn.getDate()}/${joinedOn.getMonth()}/${joinedOn.getFullYear()}`}</td>
+			<td
+				className={tdClassName}
+			>{`${currentPlanStartingDate.getDate()}/${currentPlanStartingDate.getMonth()}/${currentPlanStartingDate.getFullYear()}`}</td>
+			<td
+				className={tdClassName}
+			>{`${currentPlanEndingDate.getDate()}/${currentPlanEndingDate.getMonth()}/${currentPlanEndingDate.getFullYear()}`}</td>
 		</tr>
 	);
 }
